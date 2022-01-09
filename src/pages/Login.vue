4f992bec9471e4221f7d4c7913ec3caf580bde20
Marta <martaga.alvarez@gmail.com>
2022-01-09 21:01:11 +0100

    Feat: Create view router

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="login">
+   Login
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'Login'
+
 }
 </script>
 
